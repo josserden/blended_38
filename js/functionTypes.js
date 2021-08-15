f7e9afa2e8bdc2840cde2551c3b3f2a8f0113493
Den <52295780+josserden@users.noreply.github.com>
2021-08-15 16:42:48 +0300

    add funcTypes

---
+++ js/functionTypes.js
@@ -0,0 +1,30 @@
+// TODO Объявление функций в JS
+
+//* Function declaration
+
+// console.log(greet('John'));
+
+// function greet(name) {
+//     return `Привет, ${name} !`;
+// }
+
+//* Function expression
+
+// const greet = function (name) {
+//     return `Привет, ${name} !`;
+// };
+
+// console.log(greet('John'));
+
+//* Arrow function
+
+// const greet = name => {
+//     return `Привет, ${name} !`;
+// };
+
+// const greet = name => `Привет, ${name} !`;
+
+// console.log(greet('John'));
+
+// const sum = (a, b) => a + b;
+// console.log(sum(10, 52));
