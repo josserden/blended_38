c2283de9060c5a2179d5dcf528281c60ce6e4035
Den <52295780+josserden@users.noreply.github.com>
2021-08-29 16:52:16 +0300

    upd

---
+++ js/task.js
@@ -0,0 +1,32 @@
+// function first() {
+//     // Псевдо запрос
+
+//     setTimeout(function () {
+//         console.log(1);
+//     }, 3000);
+// }
+
+// function second() {
+//     console.log(2);
+// }
+
+// function third() {
+//     // Псевдо запрос
+
+//     setTimeout(function () {
+//         console.log(3);
+//     }, 1000);
+// }
+
+// function four() {
+//     // Псевдо запрос
+
+//     setTimeout(function () {
+//         console.log(4);
+//     }, 500);
+// }
+
+// first();
+// second();
+// third();
+// four();
