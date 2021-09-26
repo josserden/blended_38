49d8d93be02500d576a07ec39e669fdffdc0b23c
Den <52295780+josserden@users.noreply.github.com>
2021-09-26 16:43:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,14 +17,47 @@ const openModal = event => {
   lightbox.classList.add('is-open');
   imgLightbox.src = event.target.getAttribute('data-source');
   imgLightbox.alt = event.target.alt;
+
+  window.addEventListener('keydown', onPressESC);
+  gallery.addEventListener('keydown', onPressArrow);
 };
 
 const closeModal = () => {
   lightbox.classList.remove('is-open');
   imgLightbox.src = '';
   imgLightbox.alt = '';
+
+  window.removeEventListener('keydown', onPressArrow);
+};
+
+const onOverlayClick = event => {
+  if (event.target === event.currentTarget) {
+    closeModal();
+  }
+};
+
+const onPressESC = event => {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+};
+
+const onPressArrow = event => {
+  let activeIndex = galleryItems.findIndex(image => image.original === imgLightbox.src);
+
+  if (event.code === 'ArrowRight') {
+    activeIndex < galleryItems.length - 1 ? (activeIndex += 1) : 0;
+  }
+
+  if (event.code === 'ArrowLeft') {
+    activeIndex > 0 ? (activeIndex -= 1) : galleryItems.length - 1;
+  }
+
+  imgLightbox.src = galleryItems[activeIndex].original;
+  imgLightbox.alt = galleryItems[activeIndex].alt;
 };
 
 // Слушатели
 gallery.addEventListener('click', openModal);
 btnLightbox.addEventListener('click', closeModal);
+overlay.addEventListener('click', onOverlayClick);
