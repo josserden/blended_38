fc51ac59deb3608ec635c7a935ec097e2e18a155
Den <52295780+josserden@users.noreply.github.com>
2021-11-07 16:51:53 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
 
+import '../node_modules/ellipsis.js/ellipsis.min';
+
+Ellipsis({
+  ellipsis: '…', //default ellipsis value
+  responsive: true, //if you want the ellipsis to move with the window resizing
+  className: '.clamp', //default class to apply the ellipsis
+  lines: 1, //default number of lines when the ellipsis will appear
+});
+
 import { apiService } from './js/apiService';
 import news from './data/news';
 import card from './template/card.hbs';
-
-console.log(news);
 
 const formSearch = document.querySelector('.js-form');
 const gallery = document.querySelector('.js-gallery');
@@ -22,8 +29,9 @@ const onSubmit = event => {
   // });
 };
 
-const createMarkup = news.map(item => card(item)).join('');
+// const createMarkup = news.map(item => card(item)).join('');
+const createMarkup = news.reduce((acc, item) => acc + card(item), '');
 
-gallery.innerHTML = createMarkup;
+gallery.insertAdjacentHTML('beforeend', createMarkup);
 
 formSearch.addEventListener('submit', onSubmit);
